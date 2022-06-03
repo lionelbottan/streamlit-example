8b7484225c18a8a769f3dc53ab4b632b258ae352
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-03 23:37:26 +0200

    go

--- pages/1-Preprocessing.py
+++ pages/1-Preprocessing.py
@@ -4,6 +4,6 @@ st.markdown("# Page 2 ❄️")
 st.sidebar.markdown("# Page 2 ❄️")
 
 from PIL import Image
-image = Image.open('Climats.jpg')
+image = Image.open('images/Climats.jpg')
 
 st.image(image, caption='Climats')
