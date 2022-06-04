d2f1806405158948b5b11564d5e889250a2f0e1e
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-04 21:39:18 +0200

    go

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,26 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+# Streamlit encourages well-structured code, like starting execution in a main() function.
+def main():
+    # Render the readme as markdown using st.markdown.
+    readme_text = st.markdown(get_file_content_as_string("instructions.md"))
+
+
+    # Once we have the dependencies, add a selector for the app mode on the sidebar.
+    st.sidebar.title("What to do")
+    app_mode = st.sidebar.selectbox("Choose the app mode",
+        ["Show instructions", "Run the app", "Show the source code"])
+    if app_mode == "Show instructions":
+        st.sidebar.success('To continue select "Run the app".')
+    elif app_mode == "Show the source code":
+        readme_text.empty()
+        st.code(get_file_content_as_string("streamlit_app.py"))
+    elif app_mode == "Run the app":
+        readme_text.empty()
+        run_the_app()
+ 
+
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
