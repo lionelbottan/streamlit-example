cec968eb766f6e8e53849ec0deeba94221705a36
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-05 21:02:05 +0200

    go

--- pages/5-Simulation.py
+++ pages/5-Simulation.py
@@ -1,9 +1,8 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
 
 st.markdown("# Simulation")
 st.sidebar.markdown("# Simulation")
 
 model = joblib.load('modeles/xgbc_ru.joblib')
+
+picklefile = open("modeles/xgboost.pkl", "rb")
+    model = pickle.load(picklefile)
