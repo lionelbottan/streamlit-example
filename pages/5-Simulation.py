2f943f377c6310a47b0607c23a8cea6a507c87d1
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-05 21:17:28 +0200

    Update 5-Simulation.py

--- pages/5-Simulation.py
+++ pages/5-Simulation.py
@@ -2,14 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-import sklearn
-
 
 st.markdown("# Simulation")
 st.sidebar.markdown("# Simulation")
 
-picklefile = open("modeles/dt.pkl", "rb")
-model = pickle.load(picklefile)
-
 df=pd.read_csv('data/echantillon.csv') #Read our data dataset
 st.write(df.head()) 
+
+
+picklefile = open("modeles/dt.pkl", "rb")
+model = pickle.load(picklefile)
