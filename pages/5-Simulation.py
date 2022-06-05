eb53a36ea9847af9395abc86469b1d8044676d59
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-05 21:33:21 +0200

    Update 5-Simulation.py

--- pages/5-Simulation.py
+++ pages/5-Simulation.py
@@ -7,9 +7,14 @@ import sklearn
 st.markdown("# Simulation")
 st.sidebar.markdown("# Simulation")
 
+st.subheader("Lecture des données")
+
 df=pd.read_csv('data/echantillon.csv') #Read our data dataset
 st.write(df.head()) 
 
-
-picklefile = open("modeles/dt.pkl", "rb")
-model = pickle.load(picklefile)
+if st.button("Predict"):
+    picklefile = open("modeles/dt.pkl", "rb")
+    modele = pickle.load(picklefile)
+    
+    prediction = modele.predict(df)
+    st.write(prediction) 
