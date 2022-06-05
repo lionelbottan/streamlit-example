7ef194d2d0b2eedfab1c989edb842cdd985b65d6
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-05 21:35:05 +0200

    Update 5-Simulation.py

--- pages/5-Simulation.py
+++ pages/5-Simulation.py
@@ -16,5 +16,9 @@ if st.button("Predict"):
     picklefile = open("modeles/dt.pkl", "rb")
     modele = pickle.load(picklefile)
     
+    features = ["RainToday_Num","Rain_J-1","Rain_J-2","MinTemp","MaxTemp","Sunshine","Evaporation",
+            "Humidity3pm","Humidity9am","Pressure9am","Pressure3pm","Cloud3pm","Cloud9am", 
+            "Wind9am_cos","Wind3pm_cos","WindGust_cos","Wind9am_sin","Wind3pm_sin","WindGust_sin", 
+            "Mois","Clim_type_det"]
     prediction = modele.predict(df)
     st.write(prediction) 
