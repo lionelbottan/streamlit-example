0a8b1530415afa3048455827e84e188149ab5ac9
Samuel GUERIN <samuel.guerin@maaf.fr>
2022-06-07 08:32:35 +0200

    Update 5-Simulation.py

--- pages/5-Simulation.py
+++ pages/5-Simulation.py
@@ -139,6 +139,9 @@ if st.button("Predict"):
     y_pred_best = np.where( probs[:,1] >= Seuil, 1, 0)
     y_pred_best1 = np.where( probs[:,1] >= Seuil1, 1, 0)
     st.text('Matrice de confusion seuil 0.50 :\n ' + classification_report(y_test, y_pred))
+    fig = plt.figure(figsize=(15,6))
+    ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
+    st.pyplot(fig)
 #Predictions
     prediction = modele.predict(df[features])
     predDf = pd.DataFrame(prediction,columns=["prediction"])
